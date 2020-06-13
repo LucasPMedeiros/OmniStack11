ef99351b6346ee3bd3e31b48e561e6e2ed9d726e
Lucas Pereira <lucasmedeirosjf@gmail.com>
2020-06-13 06:54:40 -0300

    frontend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const OngController = require('./controllers/ong_controller')
 const IncidentController = require('./controllers/incident_controller')
 const SessiontController = require('./controllers/session_controller')
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
@@ -10,6 +11,8 @@ routes.post('/session', SessiontController.create)
 routes.post('/ongs' , OngController.create);
 routes.get('/ongs', OngController.list);
 routes.get('/specificlists', OngController.specific_lists);
+
+routes.get('/profile', ProfileController.index);
 
 routes.post('/incidents' , IncidentController.create);
 routes.get('/incidents' , IncidentController.list);
